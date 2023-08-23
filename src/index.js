8a800507769d06f3eef3b1a9596143ecc6665f98
Ra Hyeon Ji <rachaz@naver.com>
2023-08-23 23:00:26 +0900

    [Feature] | DarkMode 구현 시작(ing-) , Style-components, react-router 세팅

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 import App from './App';
-import './index.css';
+// import reportWebVitals from './reportWebVitals';
+import DarkOrLight from './component/UI/Effect/ThemeContext/DarkOrLight';
+
+// import CommentList from './component/pages/Post/comment/commentList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DarkOrLight />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')  
 );
+
+// reportWebVitals();
