c17e638415ebd47b42811ffbe72a23264ebaf5da
Ra Hyeon Ji <rachaz@naver.com>
2023-08-23 23:00:05 +0900

    [Feature] | DarkMode 구현 시작(ing-) , Style-components, react-router 세팅

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
  // App.js
 import React from 'react';
+import { useDarkMode } from './component/UI/Effect/useDarkMode/useDarkMode';
+import { ThemeProvider } from 'styled-components';
 
-const App = () => {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
+function App(){
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
   return (
-    <div className="App">
-      {/* <h1>Button Component Example</h1> */}
-      {/* <Button onClick={handleClick}>Click Me</Button> */}
-    </div>
+    <>
+        <ThemeProvider theme={themeMode}>
+          <GlobalStyles />
+          <Toggle theme ={theme} toggleTheme = {toggleTheme} />
+        </ThemeProvider>
+
+    </>
   );
-};
+}
 
 export default App;
+
+// const App = () => {
+//   const handleClick = () => {
+//     alert('Button clicked!');
+//   };
+
+//   return (
+//     <div className="App">
+//       {/* <h1>Button Component Example</h1> */}
+//       {/* <Button onClick={handleClick}>Click Me</Button> */}
+//     </div>
+//   );
+// };
+
+// export default App;
