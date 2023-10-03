5695fc5aaf30bd65465b7132bf0924f7b3168462
wanny <115992753+Subak-Uncle@users.noreply.github.com>
2023-10-04 08:59:25 +0900

    feat: 이미지 업로드 컴포넌트 제작 완료 (#43)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import MainHomePage from "./component/pages/main/mainHomePage/mainHomePage";
 import LoginPage from "./component/pages/login/loginPage/loginPage";
+// 임시 이미지 업로드
+import ImageUploadPage from "./component/pages/imageUploadPage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route exact path="/" element={<Layout />}>
             <Route index element={<MainHomePage />} />
             <Route exact path="/login" element={<LoginPage />} />
+            {/* 임시 이미지 업로드 */}
+            <Route path="/imgUpload" element={<ImageUploadPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/component/pages/imageUploadPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import ImgUpload from "../UI/organisms/ImageUpload/imageUpload";
+
+const imageUploadPage = () => {
+  const [imgList, setImgList] = useState([]);
+
+  return (
+    <>
+      <h1>이미지</h1>
+      <ImgUpload max={3} setImgList={setImgList} />
+    </>
+  );
+};
+export default imageUploadPage;
