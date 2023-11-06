39511ca26dc5de91af7eb46276f184c10630f09e
Ra Hyeon Ji <rachaz@naver.com>
2023-11-06 13:09:20 +0900

    page: 로그인 페이지 scss 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./layout/Layout";
 import AllRouters from "./Router/allRouters";
 // 임시 이미지 업로드
 import ImageUploadPage from "./component/pages/imageUploadPage";
@@ -10,14 +9,12 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <AllRouters />
         <Routes>
-          <Route exact path="/" element={<Layout />}>
-            <Route path="/" element={<AllRouters />} />
-            {/* 임시 이미지 업로드 */}
-            <Route path="/imgUpload" element={<ImageUploadPage />} />
-          </Route>
+          {/* 임시 이미지 업로드 */}
+          <Route path="/imgUpload" element={<ImageUploadPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter>{" "}
     </>
   );
 }
--- src/component/UI/organisms/Footer/footer.jsx
+++ src/component/UI/organisms/Footer/footer.jsx
@@ -8,46 +8,42 @@ function Footer() {
         <i className="logo">RUNNINGHI</i>
       </div>
 
-      <div>
-        <ul className="footer_list">
-          <li className="footer_item">
-            <a href="/" className="footer_link">
-              FAQ
-            </a>
-          </li>
-          <li className="footer_item">
-            <a href="/" className="footer_link">
-              이용약관
-            </a>
-          </li>
+      <ul className="footer_list">
+        <li className="footer_item">
+          <a href="/" className="footer_link">
+            FAQ
+          </a>
+        </li>
+        <li className="footer_item">
+          <a href="/" className="footer_link">
+            이용약관
+          </a>
+        </li>
 
-          <li className="footer_item">
-            <a href="/termsofservice" className="footer_link">
-              개인정보 처리방침
-            </a>
-          </li>
+        <li className="footer_item">
+          <a href="/termsofservice" className="footer_link">
+            개인정보 처리방침
+          </a>
+        </li>
 
-          <li className="footer_item">
-            <a href="/customersupport" className="footer_link">
-              About
-            </a>
-          </li>
-          <li className="footer_item">
-            <a href="/customerfeedback" className="footer_link">
-              Contact Us
-            </a>
-          </li>
-        </ul>
-      </div>
+        <li className="footer_item">
+          <a href="/customersupport" className="footer_link">
+            About
+          </a>
+        </li>
+        <li className="footer_item">
+          <a href="/customerfeedback" className="footer_link">
+            Contact Us
+          </a>
+        </li>
+      </ul>
 
-      {/* <div className="footerContainer">0 */}
       <div className="footer-bottom">
-        <p> RUNNINGHI All Right Reserved Copyright &copy;2023 </p>
-        <p>
+        <span> RUNNINGHI All Right Reserved Copyright &copy;2023 </span>
+        <span>
           Designed by <span className="designer">CCA-FFODREGAMDI</span>
-        </p>
+        </span>
       </div>
-      {/* </div> */}
     </footer>
   );
 }
