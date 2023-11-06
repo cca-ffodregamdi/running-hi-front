a2b3fa6f187d00fdc509361fcc358380fa515886
manje cho <whakswp123@naver.com>
2023-11-06 13:52:21 +0900

    feature: 로그인 페이지 구현 중입니다.

--- src/component/pages/login/loginPage/loginPage.jsx
+++ src/component/pages/login/loginPage/loginPage.jsx
@@ -1,6 +1,7 @@
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 import axios from "axios";
+import "../../../../../src/assets/scss/pages/login/loginPage.scss";
 
 // // 로그인 컴포넌트 정의
 // function LOGIN() {
@@ -58,6 +59,52 @@ export default class LoginPage extends Component {
     if (isLoggedIn) {
       return <div>로그인 성공!</div>;
     }
+
+    return (
+      <div className="container">
+        <div className="login-container">
+          <div className="form-container">
+            <div className="login">LOGIN</div>
+            <div className="logoname">
+              <a href="/">RUNNINGHI</a>
+            </div>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder="아이디를 입력해 주세요"
+                name="account"
+                value={this.state.account}
+                onChange={this.handleInputChange}
+              />
+              <input
+                type="password"
+                placeholder="비밀번호를 입력해 주세요"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+              />
+              <btn className="opacity" type="submit">
+                로그인
+              </btn>
+            </form>
+            <div className="register">
+              <div className="signupletter">아직 회원이 아니신가요?</div>
+              <a className="signup" href="/signup">
+                회원가입 하러가기
+              </a>
+              <div className="forget">
+                <a className="findid" href="/findid">
+                  아이디 찾기
+                </a>
+                <a className="findpw" href="/findpw">
+                  비밀번호 찾기
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 
     return (
       <div>
