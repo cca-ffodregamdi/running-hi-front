57085fab27cceb10c317ed94f411dccf5ee6f6ed
Ra Hyeon Ji <rachaz@naver.com>
2023-11-13 22:59:46 +0900

    feat : 약관동의 페이지 UI 디자인, 체크박스 로직 구현했습니다.

---
+++ src/component/UI/organisms/acceptForm/acceptForm.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "../../../../assets/scss/ui/organisms/acceptForm.scss";
+
+function AcceptForm() {
+  const [terms, setTerms] = useState({
+    serviceTerm: false,
+    privacyTerm: false,
+    serviceTerm: false,
+    allTerms: false,
+  });
+
+  const handleChange = (event) => {
+    const { name, checked } = event.target;
+
+    setTerms((prevTerms) => ({
+      ...prevTerms,
+      [name]: checked,
+      allTerms: checked
+        ? (prevTerms.serviceTerm &&
+            prevTerms.privacyTerm &&
+            name !== "allTerms") ||
+          prevTerms.allTerms
+        : false,
+    }));
+  };
+
+  const handleAllChange = (event) => {
+    const { checked } = event.target;
+    setTerms({
+      allTerms: checked,
+      serviceTerm: checked,
+      privacyTerm: checked,
+      serviceTerm: checked,
+    });
+  };
+
+  return (
+    <div className="group-checkbox">
+      <label>
+        <input
+          type="checkbox"
+          name="allTerms"
+          checked={terms.allTerms}
+          onChange={handleAllChange}
+        />
+        모든 약관 동의
+      </label>
+
+      <div className="necessary-accept">
+        <label>
+          <input
+            type="checkbox"
+            name="serviceTerm"
+            checked={terms.serviceTerm}
+            onChange={handleChange}
+          />
+          서비스 이용약관에 동의합니다 (필수)
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="privacyTerm"
+            checked={terms.privacyTerm}
+            onChange={handleChange}
+          />
+          개인정보 수집 및 이용에 동의합니다 (필수)
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="marketingTerms"
+            checked={terms.marketingTerms}
+            onChange={handleChange}
+          />
+          마케팅 정보 수신에 동의합니다 (선택)
+        </label>
+      </div>
+
+      <div className="signup-btn">
+        <button disabled={!terms.serviceTerm || !terms.privacyTerm}>
+          회원가입 하러가기
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AcceptForm;
--- src/component/pages/login/signUpPage/termsAgreePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TermsAgreePage() {
-  return <div>termsAgreePage</div>;
-}
-
-export default TermsAgreePage;
